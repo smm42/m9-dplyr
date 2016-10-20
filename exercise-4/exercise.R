3124542e928976841b37dad6571110b1efa98556
smm42 <smm42@uw.edu>
2016-10-20 15:06:38 -0700

    completed exercises

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -1,27 +1,28 @@
 # Exercise 4: DPLYR and flights data
 
 # Install the nycflights13 package and read it in.  Require the dplyr package
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # The data.frame flights should now be accessible to you.  View it, 
 # and get some basic information about the number of rows/columns
-
+View(flights)
 
 # Add a column that is the amount of time gained in the air (`arr_delay` - `dep_delay`)
-
+flights <- mutate(flights, time_gained = arr_delay - dep_delay)
 
 # Sort your data.frame desceding by the column you just created
-
+flights <- arrange(flights, time_gained)
 
 # Try doing the last 2 steps in a single operation using the pipe operator
-
+flights <- mutate(flights, time_gained = arr_delay - dep_delay) %>% arrange(time_gained)
 
 # Make a histogram of the amount of gain using the `hist` command
-
+hist(flights$time_gained)
 
 # On average, did flights gain or lose time?
+mean(flights$time_gained, na.rm = TRUE)
 
 
 # Create a data.frame that is of flights headed to seatac ('SEA'), 
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -1,17 +1,18 @@
 # Exercise 5: DPLYR Grouped Operations
 
 # Install the nycflights13 package and read it in. Require the dplyr package.
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
 
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
+View(flights)
+dep.delay.by.month <- group_by(flights, month) %>% summarise(delay = mean(dep_delay, na.rm = TRUE)) %>% arrange(-delay)
 
 # If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
 # a scatterplot by passing it to the 'plot' function
-
+plot(dep.delay.by.month)
 
 # In which airport were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
